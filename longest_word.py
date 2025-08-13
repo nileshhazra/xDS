5f14b34ebbc342481b095c781e61e6f336300d5f
Nilesh Hazra <nileshhz99@gmail.com>
2025-08-14 00:33:25 +0530

    update longest word

--- longest_word.py
+++ longest_word.py
@@ -4,10 +4,10 @@
 def longest_word(sentence):
     words = sentence.split(" ")
     longest = ""
+
     for word in words:
         if len(word) >= len(longest):
             longest = word
-            word_length = len(word)
 
     return longest
 
