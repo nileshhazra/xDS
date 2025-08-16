82820de1fcde1bed738370dd9416302619586a5b
Nilesh Hazra <nileshhz99@gmail.com>
2025-08-16 23:57:23 +0530

    update pair sum

--- pair_sum.py
+++ pair_sum.py
@@ -5,3 +5,20 @@ def pair_sum(numbers, target_sum):
         if compliment in previous:
             return (previous[compliment], i)
         previous[numbers[i]] = i
+
+
+# another approach with different syntax
+def pair_sum(numbers, target_sum):
+    previous_numbers = {}
+
+    for index, num in enumerate(numbers):
+        complement = target_sum - num
+
+        if complement in previous_numbers:
+            return (index, previous_numbers[complement])
+
+        previous_numbers[num] = index
+
+
+# time complexity - O[n]
+# space complexity - O(na)
