2017aee4b4ca9a8d2a44d4fc8e6f9d9801efb848
Nilesh Hazra <nileshhz99@gmail.com>
2025-08-17 21:22:39 +0530

    add zipper list

--- sum_list.py
+++ sum_list.py
@@ -34,3 +34,61 @@ def sum_list_r(head):
 
 
 print(sum_list_r(a))
+
+
+def linked_list_find(head, target):
+    curr = head
+    while curr is not None:
+        if curr.val == target:
+            return True
+        curr = curr.next
+    return False
+
+
+def r_linked_list_find(head, target):
+    if head is None:
+        return False
+    if head.val == target:
+        return True
+    return linked_list_find(head.next, target)
+
+
+def get_node_value(head, index):
+    curr = head
+    count = 0
+    while curr is not None:
+        if count == index:
+            return curr.val
+        count += 1
+        curr = curr.next
+
+    return None
+
+
+def get_node_value_r(head, index):
+    if head is None:
+        return None
+
+    if index == 0:
+        return head.val
+
+    return get_node_value(head.next, index - 1)
+
+
+def reverse_list(head):
+    prev = None
+    current = head
+    while current is not None:
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    return prev
+
+
+def reverse_list(head, prev=None):
+    if head is None:
+        return prev
+    next = head.next
+    head.next = prev
+    return reverse_list(next, head)
